748c1fba7ff1df6981b5a63d5512eb5ec5810617
SakshiB-29 <etudianteb.29@gmail.com>
2025-07-18 14:48:42 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Smooth scroll for nav links
-document.querySelectorAll('nav a').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    const targetId = this.getAttribute('href').substring(1);
-    const target = document.getElementById(targetId);
-    target.scrollIntoView({ behavior: 'smooth' });
-  });
-});
